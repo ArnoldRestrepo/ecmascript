17e8c893ec5761013f88208c20befc3398283eef
Arnold Restrepo <arnold.restrepo@quick.com.co>
2020-02-24 08:49:55 -0500

    Spread Operator - LET - CONST - Destructuring

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,8 @@
+/**
+ * Default Params and Concatenation
+ */
+
+
 // Before ECMAScript 6
 
 function newFunction(name, age, country){
@@ -14,5 +19,64 @@ function newFunction2(name = "Arnold", age = 22, country = "Colombia"){
 }
 
 newFunction();
-newFunction2();
+newFunction2("Ernesto Valverde", 34, "ESP");
+
+
+// Before ECMAScript 6+
+let hello = "Hello",
+    world = "World."
+// After ECMAScript 6+
+let epicPhrase = hello + world;
+console.log(epicPhrase);
+let epicPhrase2 = `${hello} ${world}`
+
+console.log(epicPhrase2);
+
+/**
+ * LET y CONST, Multiline, Spread Operator y Destructuring
+ */
+
+ // Before ECMAScript 6+
+ let lorem = "Esta es una frase y\n" + " un salto de línea";
+ // After ECMAScript 6+
+ let lorem2 = `Esta es una frase épica y además
+ un salto de línea`;
+
+ console.log(lorem, lorem2);
+
+
+let person = {
+   name: "Arnold",
+   lastName: "Restrepo",
+   age: 22
+ }
+
+ // Before ECMAScript 6+
+ console.log(person.name, person.lastName, person.age);
+ 
+// After ECMAScript 6+
+let {name, lastName, age} = person;
+
+console.log(name, lastName, age);
+
+
+let team1 = ["Juana", "Anita", "Valentina"];
+let team2 = ["Ernesto", "Jose", "Luis"];
+
+// After ECMAScript 6+
+let education = ["Ricardo", ...team1, ...team2]; 
+
+console.log(education);
+
+
+{
+  var globalVar = "Variable Global"
+} 
+
+{
+  let globalLet = "Global Let";
+  console.log(globalLet);
+}
+
+console.log(globalVar);
 
