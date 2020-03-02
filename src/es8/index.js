59c0a3c469c9173f6f07c38824ef0c1260ac462b
Arnold Restrepo <arnold.restrepo@quick.com.co>
2020-03-02 14:58:02 -0500

    ES8 - Async Await - Try Catch

--- src/es8/index.js
+++ src/es8/index.js
@@ -46,3 +46,32 @@ console.log(entries.length);
  const obj = {
    name: "Oscar",
  }
+
+
+/**
+ * Async - Await
+ */
+
+const helloWorld = () => {
+  return new Promise((resolve, reject)=>{
+    (true)
+      ? setTimeout(()=> resolve("Hello World!"), 3000)
+      : reject(new Error("Test Error"))
+  })
+}
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+}
+
+helloAsync();
+
+const AnotherFunction = async() => {
+  try{
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch(error){
+    console.error(error);
+  }
+}
